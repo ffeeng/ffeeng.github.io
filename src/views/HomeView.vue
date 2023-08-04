9fd86262b52604d953d0369374c731cfd022b8ec
fengqian <949637749@qq.com>
2023-08-04 14:55:27 +0800

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="box">
+
+    <div class="top">
+      <TopBar></TopBar>
+    </div>
+    <div class="container">
+      <LeftContent style="flex:1"></LeftContent>
+      <div class="right">
+        <FriendsLink></FriendsLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import TopBar from '@/views/TopBar.vue'
+import LeftContent from '@/views/LeftContent.vue'
+import FriendsLink from '@/components/FriendsLInks.vue'
+
+const aa = ref(1)
+</script>
+
+<style lang="scss" scoped>
+.box {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  .container {
+    flex: 1;
+    display: flex;
+
+    .left {
+      flex: 1;
+
+    }
+
+    .right {
+      width: 500px;
+    }
+  }
+
+}
+</style>
